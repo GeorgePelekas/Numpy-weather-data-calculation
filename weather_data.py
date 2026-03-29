d090c20129e7c51d34cb34a75a3612568347dcba
George Pelekas <137883580+GeorgePelekas@users.noreply.github.com>
2026-03-29 20:19:24 +0300

    Refactor weather data processing and statistics
    Updated data entries and added calculations for average, minimum, and maximum temperatures.

--- weather_data.py
+++ weather_data.py
@@ -5,20 +5,22 @@ import tempfile
 weather_structure=np.dtype([('day',np.int64),('temperature',np.float64),('humidity',np.float64)])
 fname=tempfile.mkstemp()[1]
 #creating the data 
-data=np.array([(1,22.5,60.0),(2,19.3,75.5),(3,19.3,75.5),(3,25.1,55.2),(4,17.8,80.0),(5,23.0,65.3)],dtype=weather_structure)
+data=np.array([(1,22.5,60.0),(2,19.3,75.5),(3,19.3,75.5),(4,25.1,55.2),(5,17.8,80.0),(6,23.0,65.3)],dtype=weather_structure)
 #storing the data
 data.tofile(fname)
 np.fromfile(fname,dtype=weather_structure)
 np.save(fname,data)
 #loading the data
 load=np.load(fname + ".npy")
-#showing off the AVG temperature in these 6 days
-total=0
-for row in load:
-    total+=row['temperature']
-avg_temperature=total/len(load)
-#showing the avg temperature
-print("AVG temperature from the 6 collected days was ",avg_temperature)
-
+#showing off the AVG,min,max temperature in these 6 days
+max_day=np.argmax(load['temperature'])+1
+max_temperature=load['temperature'][max_day]
+min_day=np.argmin(load['temperature'])+1
+min_temperature=load['temperature'][min_day]
+avg_temperature=np.mean(load['temperature'])
+#showing the stats of the  temperature
+print("AVG temperature from the 6 collected days was: ",avg_temperature)
+print("maximun temperature was: ",max_temperature," on day ",max_day)
+print("minimun temperature was: ",min_temperature,"on day",min_day)
 
 
